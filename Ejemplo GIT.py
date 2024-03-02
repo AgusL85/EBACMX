bf545cecb237f8cbb229c749c6397212021f6e3d
Agustín <alaraf85@gmail.com>
2024-03-01 18:15:05 -0600

    Cambio previo a Sourcetree

--- Ejemplo GIT.py
+++ Ejemplo GIT.py
@@ -1,11 +1,18 @@
 # Prueba Python
 # Se añade variable 
+# Nueva variable E
 a= 10
 b= 30
-c=30*10*5
+e=1.1
+c=30*10*5 / e
+e=1.1
 
 print("Resultado:",c)
 
-d= c*100
+# Cambiando el factor 102 Según el requerimiento del negocio
+# Cambiando el factor 100 Según Junta de Control
+# Cambio el factor a 200 según Dirección
+
+d= c* 200
 print("resultado total=",d)
 
