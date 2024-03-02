2d0d267689c1acaf9eccf5d34507f00d40a685cd
Agustín <alaraf85@gmail.com>
2024-03-02 14:01:04 -0600

    Actividad 22 (Cambios)

--- Ejemplo GIT.py
+++ Ejemplo GIT.py
@@ -2,7 +2,8 @@
 # Se añade variable 
 a= 10
 b= 30
-c=30*10*5
+e=1.1
+c=30*10*5/e
 
 print("Resultado:",c)
 
@@ -16,3 +17,7 @@ print("resultado total=",d)
 # Cambiado el factor 100 según el requerimiento del negocio
 # Cambiado a 110 el factor según junta de gobierno
 # Cambio a factor 200 por directorio
+
+#Cambio para actividad 22
+
+print("Resultado con nueva variable",e)
